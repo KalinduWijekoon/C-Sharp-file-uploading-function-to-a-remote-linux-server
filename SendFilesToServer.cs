f4abbe59ba07b5b164023839b76a16e78df6272b
Kalindu Wijekoon <reach.kalindu@gmail.com>
2020-05-12 03:11:37 +0530

    Add files via upload

---
+++ SendFilesToServer.cs
@@ -0,0 +1,20 @@
+﻿using Renci.SshNet;
+using System;
+using System.IO;
+
+namespace Sherlock
+{
+    class SendFilesToServer
+    {
+        public static void Send(string host, string username, string password, string fileName) 
+        {
+            using (ScpClient client = new ScpClient(host, username, password)) 
+            {
+                String Path = @".";
+                client.Connect();
+                client.Upload(new FileInfo(fileName), Path); 
+                client.Disconnect();                
+            }
+        }
+    }
+}
